3480ab4dd872a68e1b76ca7aa0628a9879edd2d1
John Hill <32116706+johnhill2424@users.noreply.github.com>
2017-11-07 12:28:59 -0500

    Add files via upload
    Updated cross-platform code to look the same on macs

--- GridBag.java
+++ GridBag.java
@@ -14,6 +14,24 @@ public class GridBag extends JFrame implements ActionListener{
 
    public static void main(String [] args){
       
+      try {
+            // Set cross-platform Java L&F (also called "Metal")
+        UIManager.setLookAndFeel(
+            UIManager.getCrossPlatformLookAndFeelClassName());
+      } 
+      catch (UnsupportedLookAndFeelException e) {
+       // handle exception
+      }
+      catch (ClassNotFoundException e) {
+       // handle exception
+      }
+      catch (InstantiationException e) {
+       // handle exception
+      }
+      catch (IllegalAccessException e) {
+       // handle exception
+      }
+
       new GridBag();
    
    } //End of main
