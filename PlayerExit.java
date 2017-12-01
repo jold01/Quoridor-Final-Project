a65152249f3303daf7367a2115776d0e84e451c4
DayLong1 <davez2015@gmail.com>
2017-12-01 00:28:59 -0500

    Add files via upload

---
+++ PlayerExit.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.net.*;
+import java.io.*;
+
+/**
+ * PlayerExit is a serialized class that's responsible for keeping track of players 
+ * who exit the game. <p>
+ *
+ * @author Catherine Poggioli 
+ * @author John Hill
+ * @author Jack Old
+ * @author David Luong
+ *
+ * @group# 06 
+ * @course ISTE 121
+ * @instructor Michael Floeser
+ *
+ * @version 2017-11-30
+ */
+
+public class PlayerExit implements Serializable{
+
+   /**
+    * PlayerExit default constructor
+    */   
+	public PlayerExit(){
+
+	} //End of default constructor
+
+} //End of class PlayerExit
